be974bbbc1753bb6bb777df72f55eeb3d43a5bd3
RC <robert.cantaragiu@protonmail.com>
2021-05-24 16:23:49 +0300

    syntax corrections

--- selnmpy.py
+++ selnmpy.py
@@ -14,30 +14,30 @@ import getpass
 
 DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 NORMAL_LOAD_AMMOUNT = 3
-1_HOUR = 3600
+ONE_HOUR = 3600
 
 def rand_dist():
 	rand_number = random.randint(1,23)
 	if rand_number in [1,2,3]:
-		return random.randint(10,1_HOUR)
+		return random.randint(10,ONE_HOUR)
 	elif rand_number in [4,5,6]:
-		return random.randint(1_HOUR,2*1_HOUR)
+		return random.randint(ONE_HOUR,2*ONE_HOUR)
 	elif rand_number in [7,8,9,10]:
-		return random.randint(2*1_HOUR,3*1_HOUR)
+		return random.randint(2*ONE_HOUR,3*ONE_HOUR)
 	elif rand_number in [11,12,13]:
-		return random.randint(3*1_HOUR,4*1_HOUR)
+		return random.randint(3*ONE_HOUR,4*ONE_HOUR)
 	elif rand_number in [14,15,16]:
-		return random.randint(4*1_HOUR,5*1_HOUR)
+		return random.randint(4*ONE_HOUR,5*ONE_HOUR)
 	elif rand_number in [17,18]:
-		return random.randint(5*1_HOUR,6*1_HOUR)
+		return random.randint(5*ONE_HOUR,6*ONE_HOUR)
 	elif rand_number in [19,20]:
-		return random.randint(6*1_HOUR,7*1_HOUR)
+		return random.randint(6*ONE_HOUR,7*ONE_HOUR)
 	elif rand_number in [21]:
-		return random.randint(7*1_HOUR,8*1_HOUR)
+		return random.randint(7*ONE_HOUR,8*ONE_HOUR)
 	elif rand_number in [22]:
-		return random.randint(8*1_HOUR,9*1_HOUR)
+		return random.randint(8*ONE_HOUR,9*ONE_HOUR)
 	elif rand_number in [23]:
-		return random.randint(9*1_HOUR,10*1_HOUR)
+		return random.randint(9*ONE_HOUR,10*ONE_HOUR)
 
 def rand_fb_site():
 	marketplace = 'https://www.facebook.com/marketplace/?ref=bookmark'
@@ -335,7 +335,7 @@ def main():
 	fx_prof = webdriver.FirefoxProfile(profile_path)
 
 	#exec_path = input("Enter geckodriver executable path:")
-	exec_path = "/home/asd/Downloads/geckodriver/geckodriver"
+	exec_path = '/home/'+ os.getlogin() + '/Downloads/geckodriver/geckodriver'
 	driver = webdriver.Firefox(executable_path = exec_path,firefox_profile = fx_prof)
 	
 	rand_ste = rand_fb_site()
